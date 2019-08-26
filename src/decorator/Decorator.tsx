910ca0706e3f864c27054798176f24c32f3ca8b9
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-08-26 14:03:23 +0200

    fjern ubrukt kode

--- src/decorator/Decorator.tsx
+++ src/decorator/Decorator.tsx
@@ -4,16 +4,16 @@ import { RouteComponentProps, withRouter } from 'react-router';
 import { AppRoute, hentRoute, MatchProps } from '../utils/paths';
 
 interface DecoratorProps {
-    appname: string; // Navn på applikasjon
-    fnr: string | undefined | null; // Fødselsnummer på bruker i context. NB, endring av denne medfører oppdatering av context
-    enhet: string | undefined | null; // Enhetsnummer på enhet i context. NB, endring av denne medfører oppdatering av context
-    toggles: Toggles; // Konfigurasjon av hvile elementer som skal vises i dekoratøren
-    markup?: Markup; // Ekstra innhold i dekoratøren, kan brukes om man trenger å legge en knapp innenfor dekoratøren
+    appname: string; 
+    fnr: string | undefined | null; 
+    enhet: string | undefined | null; 
+    toggles: Toggles; 
+    markup?: Markup; 
 
-    onSok(fnr: string): void; // Callback-funksjon for når man skal bytte bruker (blir kalt etter bekreftelse-modal, eller ved direkte søk i søkefeltet)
+    onSok(fnr: string): void; 
 
-    onEnhetChange(enhet: string): void; // Callback-funksjon for når man skal bytte enhet (blir kalt etter beksreftelse-modal, eller ved direkte endring i enhets-dropdown)
-    contextholder?: true | Contextholder; // Konfigurasjn av tilkobling til contextholder. true; use default. Om man sender inn objekt så kan man overstyre url og om enhet skal generere bekreftelsemodal. Om den ikke settes vil man ikke bruke contextholder.
+    onEnhetChange(enhet: string): void; 
+    contextholder?: true | Contextholder; 
 }
 
 interface Toggles {
@@ -25,24 +25,11 @@ interface Toggles {
 
 interface Contextholder {
     url?: string;
-    promptBeforeEnhetChange?: boolean; // Kan settes om man ikke ønsker bekreftelse-modal ved enhets-endringer
+    promptBeforeEnhetChange?: boolean; 
 }
 
 interface Markup {
     etterSokefelt?: string;
-}
-
-export interface Saksbehandler {
-    ident: string;
-    fornavn: string;
-    etternavn: string;
-    navn: string;
-    enheter: Array<Enhet>;
-}
-
-interface Enhet {
-    enhetId: string;
-    navn: string;
 }
 
 const InternflateDecorator = NAVSPA.importer<DecoratorProps>('internarbeidsflatefs');
