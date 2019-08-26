8bc547da031f642338ab963b7e3ad1888faeb436
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-08-26 15:08:53 +0200

    Logikk for aktørid

--- src/decorator/Decorator.tsx
+++ src/decorator/Decorator.tsx
@@ -1,7 +1,8 @@
 import NAVSPA from '@navikt/navspa';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import { RouteComponentProps, withRouter } from 'react-router';
 import { AppRoute, hentRoute, MatchProps } from '../utils/paths';
+import { hentAktørId, hentFnr } from '../api/finnKandidatApi';
 
 interface DecoratorProps {
     appname: string; 
@@ -34,28 +35,43 @@ interface Markup {
 
 const InternflateDecorator = NAVSPA.importer<DecoratorProps>('internarbeidsflatefs');
 
-// TODO Dekoratøren prøver å koble seg til contextholder i Modia, som lagrer hvilken bruker og enhet veileder jobber på.
-// Den prøver seg på /modiacontextholder, men der ligger den ikke. Denne pathen er utenfor appens domene, så vi kan
-// heller ikke legge på proxy. Må nok gjøre endringer i selve dekoratøren.
+const Decorator: FunctionComponent<RouteComponentProps<MatchProps>> = props => {
+    const [fnr, setFnr] = useState<string>('');
 
-const Decorator: FunctionComponent<RouteComponentProps<MatchProps>> = props => (
-    <InternflateDecorator
-        appname={'“Tilrettelegger’n”'}
-        fnr={props.match.params.fnr}
-        enhet={null}
-        toggles={{
-            visVeilder: false,
-            visSokefelt: true,
-            visEnhetVelger: false,
-            visEnhet: false,
-        }}
-        onSok={fnr => {
-            // TODO Her må vi konvertere fra fnr til aktørId når den tid kommer.
-            const pathTilPerson = hentRoute(AppRoute.SeKandidat, fnr);
-            props.history.push(pathTilPerson);
-        }}
-        onEnhetChange={enhet => {}}
-    />
-);
+    const aktørId = props.match.params.aktorId;
+
+    useEffect(() => {
+        const hentOgSettFnr = async () => {
+            try {
+                const respons = await hentFnr(aktørId);
+                setFnr(respons.data);
+            } catch (error) {
+                setFnr('');
+            }
+        };
+        hentOgSettFnr();
+    }, [aktørId]);
+
+    return (
+        <InternflateDecorator
+            appname={'“Tilrettelegger’n”'}
+            fnr={fnr}
+            enhet={null}
+            toggles={{
+                visVeilder: false,
+                visSokefelt: true,
+                visEnhetVelger: false,
+                visEnhet: false,
+            }}
+            onSok={async (fnr) => {
+                const aktørIdResponse = await hentAktørId(fnr);
+                const pathTilPerson = hentRoute(AppRoute.SeKandidat, aktørIdResponse.data);
+                props.history.push(pathTilPerson);
+            }}
+            onEnhetChange={enhet => {
+            }}
+        />
+    );
+}
 
 export default withRouter(Decorator);
