d61f1fcf70d7da246c6b726c4f554a2bcc380f82
Lars Andreas Tveiten <Lars.Andreas.van.Woensel.Kooy.Tveiten@nav.no>
2019-08-27 16:35:25 +0200

    mocking med craco

--- craco.config.js
+++ craco.config.js
@@ -13,4 +13,17 @@ module.exports = {
             },
         },
     },
+    devServer: {
+        proxy: {
+            '/finn-kandidat/decorator/v2/static/js/head.v2.min.js': {
+                target: 'http://localhost:8080',
+                pathRewrite: {'^/ditt-nav-arbeidsgiver/api' : '/ditt-nav-arbeidsgiver-api/api'}
+            }
+        },
+        before: (app) => {
+            app.get('/finn-kandidat/decorator/v2/static/js/head.v2.min.js', (req, res) => {
+                res.send("test.js")
+            });
+        }
+    },
 };
