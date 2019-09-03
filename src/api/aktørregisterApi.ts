6a7c56d7a211c56665d5af800ae53d403df4b6d1
André Mossige <mossige.a@gmail.com>
2019-09-03 10:38:20 +0200

    Legg til api kall for å hente fnr

--- src/api/aktørregisterApi.ts
+++ src/api/aktørregisterApi.ts
@@ -5,15 +5,26 @@ import { randomCallId } from '../utils/randomIdUtils';
 const aktørIdUrl =
     '/finn-kandidat/aktoerregister/api/v1/identer?identgruppe=AktoerId&gjeldende=true';
 
+const fnrUrl = '/finn-kandidat/aktoerregister/api/v1/identer?identgruppe=NorskIdent&gjeldende=true';
+
 export const hentAktørId = async (fnr: string): Promise<AxiosResponse<AktorIdResponse>> => {
     return await axios.get(aktørIdUrl, {
         withCredentials: true,
-        headers: {
-            'Nav-Consumer-Id': 'finn-kandidat',
-            'Nav-Call-Id': randomCallId(),
-            'Nav-Personidenter': fnr,
-        },
+        headers: lagHeaders(fnr),
     });
 };
 
-// TODO: hentFnr
+export const hentFnr = async (aktørId: string): Promise<AxiosResponse<AktorIdResponse>> => {
+    return await axios.get(fnrUrl, {
+        withCredentials: true,
+        headers: lagHeaders(aktørId),
+    });
+};
+
+const lagHeaders = (ident: string) => {
+    return {
+        'Nav-Consumer-Id': 'finn-kandidat',
+        'Nav-Call-Id': randomCallId(),
+        'Nav-Personidenter': ident,
+    };
+};
