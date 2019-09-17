cd20f787797f04f205987526a4ebed50a5a41fc9
André Mossige <mossige.a@gmail.com>
2019-09-17 10:31:00 +0200

    Mer logging

--- src/api/aktørregister/initialiserAktørregisterApi.ts
+++ src/api/aktørregister/initialiserAktørregisterApi.ts
@@ -31,18 +31,37 @@ api.interceptors.response.use(
     },
     error => {
         console.log('feil i aktørregisterinterceptor. error: ', error);
-        if (error.response.status === 403) {
-            if (process.env.REACT_APP_MOCK) {
-                console.log('redirecter til openam');
-            } else {
-                console.log('403 feil fra aktørregister, redirecter til OpenAM. error: ', error);
-                redirectTilOpenAMLogin();
-            }
+        if (error.response) {
+            console.log('error.response');
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+        } else if (error.request) {
+            console.log('error.request');
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
         } else {
-            console.log('annen feil fra atkørregister, redirecter til OpenAM. error: ', error);
-            redirectTilOpenAMLogin();
-            return Promise.reject(error);
+            // Something happened in setting up the request that triggered an Error
+            console.log('else Error', error.message);
         }
+        console.log('error.config');
+        console.log(error.config);
+        // if (error.response.status === 403) {
+        //     if (process.env.REACT_APP_MOCK) {
+        //         console.log('redirecter til openam');
+        //     } else {
+        //         console.log('403 feil fra aktørregister, redirecter til OpenAM. error: ', error);
+        //         redirectTilOpenAMLogin();
+        //     }
+        // } else {
+        //     console.log('annen feil fra atkørregister, redirecter til OpenAM. error: ', error);
+        //     redirectTilOpenAMLogin();
+        //     return Promise.reject(error);
+        // }
     }
 );
 
