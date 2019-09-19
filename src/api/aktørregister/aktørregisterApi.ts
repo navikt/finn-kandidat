181a489ecae1f8d9445888504b84ec8b016b3321
André Mossige <mossige.a@gmail.com>
2019-09-19 12:26:40 +0200

    Redirect til veilarblogin i test

--- src/api/aktørregister/aktørregisterApi.ts
+++ src/api/aktørregister/aktørregisterApi.ts
@@ -10,12 +10,14 @@ export const fnrUrl = `${environment.AKTORREGISTER_URL}/identer?identgruppe=Nors
 export const hentAktørId = async (fnr: string): Promise<AxiosResponse<AktorIdResponse>> => {
     return await aktørregisterApi.get(aktørIdUrl, {
         headers: lagHeaders(fnr),
+        withCredentials: true,
     });
 };
 
 export const hentFnr = async (aktørId: string): Promise<AxiosResponse<AktorIdResponse>> => {
     return await aktørregisterApi.get(fnrUrl, {
         headers: lagHeaders(aktørId),
+        withCredentials: true,
     });
 };
 
--- src/api/aktørregister/initialiserAktørregisterApi.ts
+++ src/api/aktørregister/initialiserAktørregisterApi.ts
@@ -14,14 +14,17 @@ const redirectTilOpenAMLogin = () => {
         );
         // TODO: sett expires
         // TODO: Test ut universal-cookie pakke om dette ikke fungerer
-        // const cookie = `state_${randomId}=${redirectUrl}; domain=.adeo.no; path=/veilarblogin/api/login; max-age=3600`;
+        // const cookie = `state_${randomId}=${redirectUrl}; domain=adeo.no; path=/veilarblogin/api/login; max-age=3600`;
+        // const cookie = `state_${randomId}=${redirectUrl}; domain=nrk.no; path=/veilarblogin/api/login; max-age=3600`;
         // document.cookie = cookie;
         // const openAmUrl = `https://isso-q.adeo.no/isso/oauth2/authorize?session=winssochain&authIndexType=service&authIndexValue=winssochain&response_type=code&scope=openid&client_id=veilarblogin-q0&state=state_${randomId}&redirect_uri=https://app-q0.adeo.no/veilarblogin/api/login`;
+        // window.location.href = openAmUrl;
         // console.log(cookie);
         // console.log(openAmUrl);
         //
         // const cookies = new Cookies();
-        // TODO: Kan ikke sette annet domain enn det man er på
+        // TODO: Kan ikke sette annet domain enn det man er på?
+        //  domene skal ikke ha leading .
         // cookies.set(`state_${randomId}`, redirectUrl, {
         //     domain: '.adeo.no',
         //     path: '/veilarblogin/api/login',
@@ -39,10 +42,14 @@ const redirectTilOpenAMLogin = () => {
         // console.log('cookies');
         // console.log(cookies.getAll());
         // console.log(document.cookie);
+
+        // hvis man ikke kan bytte cookie domain må man kanskje redirecte til /start endepunktet som bare fins i testmiljø?
         window.location.href = 'https://app-q0.adeo.no/veilarblogin/api/start?url=' + redirectUrl;
     } else {
+        // TODO: if (url inneholder noe greier) test ut
         const redirectUrl = encodeURIComponent('https://arbeidsgiver.nais.adeo.no/finn-kandidat/');
-        document.cookie = `state_${randomId}=${redirectUrl}; domain=.adeo.no; path=/veilarblogin/api/login; max-age=3600`;
+        document.cookie = `state_${randomId}=${redirectUrl}; domain=adeo.no; path=/veilarblogin/api/login; max-age=3600`;
+        // TODO: Kan bruke Set-cookie på noe vis?
         window.location.href = `https://isso.adeo.no/isso/oauth2/authorize?session=winssochain&authIndexType=service&authIndexValue=winssochain&response_type=code&scope=openid&client_id=veilarblogin-p&state=state_${randomId}&redirect_uri=https://app.adeo.no/veilarblogin/api/login`;
     }
 };
@@ -58,28 +65,45 @@ api.interceptors.response.use(
         return response;
     },
     (error: AxiosError) => {
-        // console.log('feil i aktørregisterinterceptor. error:');
-        // console.log(error);
-        // console.log('error.message');
-        // console.log(error.message);
-        // console.log('error.code');
-        // console.log(error.code);
-        // console.log('error.config');
-        // console.log(error.config);
-        // console.log('error.isAxiosError');
-        // console.log(error.isAxiosError);
-        // console.log('error.request');
-        // console.log(error.request);
-        // console.log('error.name');
-        // console.log(error.name);
+        console.log('feil i aktørregisterinterceptor. error:');
+        console.log(error);
+        console.log('error.message');
+        console.log(error.message);
+        console.log('error.code');
+        console.log(error.code);
+        console.log('error.config');
+        console.log(error.config);
+        console.log('error.request');
+        console.log(error.request);
+        console.log('error.name');
+        console.log(error.name);
+        console.log('error.response');
+        console.log(error.response);
+        console.log('-------------');
+        console.log('-------------');
 
-        console.log('redirecter til openam');
-        redirectTilOpenAMLogin();
+        // hvis 403
+        if (error.response && error.response.status === 403) {
+            console.log('error 403 fra aktørregister');
+            console.log('redirecter til openam');
+            redirectTilOpenAMLogin();
+            // hvis cors feil. Typ 403 på pre flight kall
+        } else if (!error.response) {
+            console.log('cors feil?');
+            console.log('error.message');
+            console.log(error.message.toString());
 
-        return Promise.reject(error);
+            console.log('redirecter til openam');
+            redirectTilOpenAMLogin();
+            // hvis ikke
+        } else {
+            console.log('else promise reject');
+            return Promise.reject(error);
+        }
 
         // Angående å få vite om det er CORS feil eller ikke
         // https://github.com/axios/axios/issues/838
+        // https://github.com/axios/axios/issues/383
         // Må kanskje håndtere feilmeldinger, og hvis ingenting funker vi må vi redirecte til OpenAM
 
         // if (error.response.status === 403) {
